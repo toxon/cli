306913acb570200f5ef583718def28c911b53e3c
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-21 16:16:49 +0000

    Display chat

--- lib/widgets/chat.rb
+++ lib/widgets/chat.rb
@@ -3,6 +3,7 @@
 module Widgets
   class Chat
     attr_reader :x, :y, :width, :height
+    attr_reader :messages
 
     def initialize(x, y, width, height)
       @x = x
@@ -10,9 +11,54 @@ module Widgets
 
       @width  = width
       @height = height
+
+      @messages = 1.upto(100).map do
+        {
+          time: Faker::Time.forward,
+          name: Faker::Name.name,
+          text: Faker::Lorem.sentence.freeze,
+        }
+      end
     end
 
     def render
+      offset = 0
+
+      messages.each do |msg|
+        time = msg[:time].strftime '%H:%M:%S'
+        name = msg[:name]
+        text = msg[:text]
+
+        offset = render_message offset, time, name, text
+
+        break if offset >= height
+      end
+    end
+
+    def render_message(offset, time, name, text)
+      Curses.attron Curses.color_pair 1
+      Curses.setpos y + offset, x
+
+      info_length = time.length + 1 + name.length + 2
+      head_length = width - info_length
+      head = text[0...head_length]
+
+      Curses.addstr time
+      Curses.addstr ' '
+      Curses.addstr name
+      Curses.addstr ': '
+      Curses.addstr head
+
+      tail_length = [0, text.length - head_length].max
+      tail = text[head_length..-1]
+      lines = (tail_length / width.to_f).ceil
+
+      1.upto lines do |line|
+        Curses.setpos y + offset + line, x
+        Curses.addstr tail[(width * (line - 1))...(width * line)]
+      end
+
+      offset + 1 + lines
     end
   end
 end
