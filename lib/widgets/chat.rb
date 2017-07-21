9f87a76bd46e112b7e62f3b2afbe44ccf4a42a91
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-21 18:08:16 +0000

    Move code from Widget::Chat to Widget::Chat:History

--- lib/widgets/chat.rb
+++ lib/widgets/chat.rb
@@ -4,7 +4,6 @@ module Widgets
   class Chat
     attr_reader :x, :y, :width, :height
     attr_reader :focused
-    attr_reader :messages
 
     def initialize(x, y, width, height)
       @x = x
@@ -14,68 +13,14 @@ module Widgets
       @height = height
 
       @message = NewMessage.new x, y + height - 1, width, 1
-      @history = History.new    x, y,              width, history_height
+      @history = History.new    x, y,              width, height - 1
 
       @focused = false
-
-      @messages = 1.upto(100).map do
-        {
-          time: Faker::Time.forward,
-          name: Faker::Name.name,
-          text: Faker::Lorem.sentence.freeze,
-        }
-      end
-    end
-
-    def history_height
-      height - 1
     end
 
     def render
       @history.render
       @message.render
-
-      offset = 0
-
-      messages.each do |msg|
-        time = msg[:time].strftime '%H:%M:%S'
-        name = msg[:name]
-        text = msg[:text]
-
-        offset = render_message offset, time, name, text
-
-        break if offset >= history_height
-      end
-    end
-
-    def render_message(offset, time, name, text)
-      Curses.setpos y + offset, x
-
-      info_length = time.length + 1 + name.length + 2
-      head_length = width - info_length
-      head = text[0...head_length]
-
-      Curses.attron Curses.color_pair 6
-      Curses.addstr time
-      Curses.addstr ' '
-      Curses.attron Curses.color_pair 7
-      Curses.attron Curses::A_BOLD
-      Curses.addstr name
-      Curses.addstr ': '
-      Curses.attroff Curses::A_BOLD
-      Curses.attron Curses.color_pair 1
-      Curses.addstr head
-
-      tail_length = [0, text.length - head_length].max
-      tail = text[head_length..-1]
-      lines = (tail_length / width.to_f).ceil
-
-      1.upto lines do |line|
-        Curses.setpos y + offset + line, x
-        Curses.addstr tail[(width * (line - 1))...(width * line)]
-      end
-
-      offset + 1 + lines
     end
 
     def trigger(event)
--- lib/widgets/chat/history.rb
+++ lib/widgets/chat/history.rb
@@ -6,6 +6,8 @@ module Widgets
       attr_reader :x, :y, :width, :height
       attr_accessor :focused
 
+      attr_reader :messages
+
       def initialize(x, y, width, height)
         @x = x
         @y = y
@@ -14,9 +16,59 @@ module Widgets
         @height = height
 
         @focused = false
+
+        @messages = 1.upto(100).map do
+          {
+            time: Faker::Time.forward,
+            name: Faker::Name.name,
+            text: Faker::Lorem.sentence.freeze,
+          }
+        end
       end
 
-      def render; end
+      def render
+        offset = 0
+
+        messages.each do |msg|
+          time = msg[:time].strftime '%H:%M:%S'
+          name = msg[:name]
+          text = msg[:text]
+
+          offset = render_message offset, time, name, text
+
+          break if offset >= height
+        end
+      end
+
+      def render_message(offset, time, name, text)
+        Curses.setpos y + offset, x
+
+        info_length = time.length + 1 + name.length + 2
+        head_length = width - info_length
+        head = text[0...head_length]
+
+        Curses.attron Curses.color_pair 6
+        Curses.addstr time
+        Curses.addstr ' '
+        Curses.attron Curses.color_pair 7
+        Curses.attron Curses::A_BOLD
+        Curses.addstr name
+        Curses.addstr ': '
+        Curses.attroff Curses::A_BOLD
+        Curses.attron Curses.color_pair 1
+        Curses.addstr head
+
+        tail_length = [0, text.length - head_length].max
+        tail = text[head_length..-1]
+        lines = (tail_length / width.to_f).ceil
+
+        1.upto lines do |line|
+          Curses.setpos y + offset + line, x
+          Curses.addstr tail[(width * (line - 1))...(width * line)]
+        end
+
+        offset + 1 + lines
+      end
 
       def trigger(event); end
     end
