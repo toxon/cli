af5f7bcb4f0d91756c035ba92866c92f442c1550
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-21 15:05:10 +0000

    Big menu items

--- lib/widgets/menu.rb
+++ lib/widgets/menu.rb
@@ -21,8 +21,15 @@ module Widgets
     def render
       ITEMS.each_with_index do |item, index|
         Curses.attron Curses.color_pair 5
+
+        Curses.setpos 1 + y + index * 4, 2
+        Curses.addstr ' ' * (width - 4)
+
         Curses.setpos 2 + y + index * 4, 2
-        Curses.addstr item
+        Curses.addstr " #{item}".ljust width - 4
+
+        Curses.setpos 3 + y + index * 4, 2
+        Curses.addstr ' ' * (width - 4)
       end
     end
   end
