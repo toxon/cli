ae7c16ad85d5da8f24251f0b53ca10ded82c3335
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-22 16:02:52 +0000

    Add method Widgets::Menu::Items

--- lib/widgets/menu/items.rb
+++ lib/widgets/menu/items.rb
@@ -11,16 +11,20 @@ module Widgets
 
       def draw
         ITEMS.each_with_index do |item, index|
-          Style.default.menu_item do
-            setpos 2, 4 * index + 0
-            Curses.addstr ' ' * (width - 4)
+          draw_item index, item
+        end
+      end
 
-            setpos 2, 4 * index + 1
-            Curses.addstr " #{item}".ljust width - 4
+      def draw_item(index, name)
+        Style.default.menu_item do
+          setpos 2, 4 * index + 0
+          Curses.addstr ' ' * (width - 4)
 
-            setpos 2, 4 * index + 2
-            Curses.addstr ' ' * (width - 4)
-          end
+          setpos 2, 4 * index + 1
+          Curses.addstr " #{name}".ljust width - 4
+
+          setpos 2, 4 * index + 2
+          Curses.addstr ' ' * (width - 4)
         end
       end
     end
