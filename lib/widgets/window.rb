ed6ed244b6adefc11d23c8e3cb3401ca26339a56
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-22 17:22:17 +0000

    Fix code style

--- lib/widgets/window.rb
+++ lib/widgets/window.rb
@@ -5,8 +5,19 @@ module Widgets
     def initialize(x, y, width, height)
       super
 
-      @menu      = Widgets::Menu.new      0,           0, nil,                              Curses.stdscr.maxy
-      @messenger = Widgets::Messenger.new @menu.width, 0, Curses.stdscr.maxx - @menu.width, Curses.stdscr.maxy
+      @menu = Widgets::Menu.new(
+        0,
+        0,
+        nil,
+        Curses.stdscr.maxy,
+      )
+
+      @messenger = Widgets::Messenger.new(
+        @menu.width,
+        0,
+        Curses.stdscr.maxx - @menu.width,
+        Curses.stdscr.maxy,
+      )
 
       self.focus = @menu
     end
