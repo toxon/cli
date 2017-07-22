241bf568019a7bacc4a5fcce31854290478446f0
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-22 09:38:38 +0000

    Configure colors via methods

--- lib/style.rb
+++ lib/style.rb
@@ -7,12 +7,68 @@ class Style
   end
 
   def initialize
-    Curses.init_pair self.class.counter, Curses::COLOR_WHITE, Curses::COLOR_BLACK # text
-    Curses.init_pair self.class.counter, Curses::COLOR_BLACK, Curses::COLOR_WHITE # selection
-    Curses.init_pair self.class.counter, Curses::COLOR_WHITE, Curses::COLOR_BLACK # editing text
-    Curses.init_pair self.class.counter, Curses::COLOR_BLACK, Curses::COLOR_GREEN # cursor
-    Curses.init_pair self.class.counter, Curses::COLOR_BLACK, Curses::COLOR_CYAN  # menu item
-    Curses.init_pair self.class.counter, Curses::COLOR_CYAN,  Curses::COLOR_BLACK # message time
-    Curses.init_pair self.class.counter, Curses::COLOR_GREEN, Curses::COLOR_BLACK # message author
+    Curses.init_pair self.class.counter, text_color,           text_bg
+    Curses.init_pair self.class.counter, selection_color,      selection_bg
+    Curses.init_pair self.class.counter, editing_text_color,   editing_text_bg
+    Curses.init_pair self.class.counter, cursor_color,         cursor_bg
+    Curses.init_pair self.class.counter, menu_item_color,      menu_item_bg
+    Curses.init_pair self.class.counter, message_time_color,   message_time_bg
+    Curses.init_pair self.class.counter, message_author_color, message_author_bg
+  end
+
+  def text_color
+    Curses::COLOR_WHITE
+  end
+
+  def text_bg
+    Curses::COLOR_BLACK
+  end
+
+  def selection_color
+    Curses::COLOR_BLACK
+  end
+
+  def selection_bg
+    Curses::COLOR_WHITE
+  end
+
+  def editing_text_color
+    Curses::COLOR_WHITE
+  end
+
+  def editing_text_bg
+    Curses::COLOR_BLACK
+  end
+
+  def cursor_color
+    Curses::COLOR_BLACK
+  end
+
+  def cursor_bg
+    Curses::COLOR_GREEN
+  end
+
+  def menu_item_color
+    Curses::COLOR_BLACK
+  end
+
+  def menu_item_bg
+    Curses::COLOR_CYAN
+  end
+
+  def message_time_color
+    Curses::COLOR_CYAN
+  end
+
+  def message_time_bg
+    Curses::COLOR_BLACK
+  end
+
+  def message_author_color
+    Curses::COLOR_GREEN
+  end
+
+  def message_author_bg
+    Curses::COLOR_BLACK
   end
 end
