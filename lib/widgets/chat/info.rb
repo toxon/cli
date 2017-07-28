f492e9cc5730d214874ff7036db4bbdac21f315d
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-28 12:01:48 +0000

    Render status with elements

--- lib/widgets/chat/info.rb
+++ lib/widgets/chat/info.rb
@@ -10,24 +10,22 @@ module Widgets
     private
 
       def render
-        setpos 0, 0
+        Curses::React::Nodes.create(
+          create_element(:wrapper) do
+            create_element :wrapper do
+              create_element :text_line,
+                             x: 0,
+                             y: 0,
+                             width: status_text.length,
+                             text: status_text,
+                             attr: status_attr
+            end
+          end,
 
-        case props[:status]
-        when Tox::UserStatus::NONE
-          Style.default.online_mark window do
-            addstr '[Online]'
-          end
-        when Tox::UserStatus::AWAY
-          Style.default.away_mark window do
-            addstr '[Away]'
-          end
-        when Tox::UserStatus::BUSY
-          Style.default.busy_mark window do
-            addstr '[Busy]'
-          end
-        else
-          addstr '[Unknown]'
-        end
+          window,
+        ).draw
+
+        setpos status_text.length, 0
 
         addstr ' '
         Style.default.peer_info_name window do
@@ -55,6 +53,23 @@ module Widgets
 
           window,
         ).draw
+      end
+
+      def status_text
+        case props[:status]
+        when Tox::UserStatus::NONE then '[Online]'
+        when Tox::UserStatus::AWAY then '[Away]'
+        when Tox::UserStatus::BUSY then '[Busy]'
+        else '[Unknown]'
+        end
+      end
+
+      def status_attr
+        case props[:status]
+        when Tox::UserStatus::NONE then Style.default.online_mark_attr
+        when Tox::UserStatus::AWAY then Style.default.away_mark_attr
+        when Tox::UserStatus::BUSY then Style.default.busy_mark_attr
+        end
       end
     end
   end
