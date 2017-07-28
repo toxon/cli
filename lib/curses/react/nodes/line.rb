a1ea94436e943f47e217c2f65c2cd84d687b8f8e
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-28 13:35:25 +0000

    Curses::React::Nodes::Lines passes width to child nodes

--- lib/curses/react/nodes/line.rb
+++ lib/curses/react/nodes/line.rb
@@ -4,6 +4,19 @@ module Curses
   module React
     module Nodes
       class Line < Wrapper
+        attr_reader :width
+
+        def initialize(element, window, width:)
+          super element, window
+          self.width = width
+        end
+
+      private
+
+        def width=(value)
+          raise TypeError, "expected width to be an #{Integer}" unless value.is_a? Integer
+          @width = value
+        end
       end
     end
   end
--- lib/curses/react/nodes/lines.rb
+++ lib/curses/react/nodes/lines.rb
@@ -4,6 +4,13 @@ module Curses
   module React
     module Nodes
       class Lines < Wrapper
+        def children
+          props[:children].map do |child_element|
+            node_klass = Nodes.klass_for child_element
+            raise "#{self.class} can only have children of type #{Line}" unless node_klass <= Line
+            node_klass.new child_element, @window, width: props[:width]
+          end
+        end
       end
     end
   end
