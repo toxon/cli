641dd6c192327924e1a850b3c0a21b61cdb70954
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-28 16:43:41 +0000

    Wrap lines

--- lib/widgets/chat/history.rb
+++ lib/widgets/chat/history.rb
@@ -33,43 +33,44 @@ module Widgets
         lines = (text.length / width.to_f).ceil
 
         elem = render_lines offset, out, text, width, left, lines
-        Curses::React::Nodes.klass_for(elem).new(
-          elem,
-          window,
-        ).draw
+        Curses::React::Nodes.klass_for(elem).new(elem, window).draw
 
-        elem = render_header error, out, name, time
         width = name.length + time.length + (error ? 3 : 1)
-        Curses::React::Nodes.klass_for(elem).new(
-          elem,
-          window,
+        elem = render_header(
+          error,
+          out,
+          name,
+          time,
           x: out ? props[:width] - width : 0,
           y: props[:height] - offset - lines - 1,
           width: width,
-        ).draw
+        )
+        Curses::React::Nodes.klass_for(elem).new(elem, window).draw
 
         1 + lines
       end
 
-      def render_header(error, out, name, time)
-        create_element :line do
-          if out
-            if error
-              create_element :text, text: 'x', attr: Style.default.message_error_attr
-              create_element :text, text: ' '
-            end
+      def render_header(error, out, name, time, x:, y:, width:)
+        create_element :lines, x: x, y: y, width: width do
+          create_element :line do
+            if out
+              if error
+                create_element :text, text: 'x', attr: Style.default.message_error_attr
+                create_element :text, text: ' '
+              end
 
-            create_element :text, text: name, attr: Style.default.message_author_attr
-            create_element :text, text: ' '
-            create_element :text, text: time, attr: Style.default.message_time_attr
-          else
-            create_element :text, text: time, attr: Style.default.message_time_attr
-            create_element :text, text: ' '
-            create_element :text, text: name, attr: Style.default.message_author_attr
-
-            if error
+              create_element :text, text: name, attr: Style.default.message_author_attr
               create_element :text, text: ' '
-              create_element :text, text: 'x', attr: Style.default.message_error_attr
+              create_element :text, text: time, attr: Style.default.message_time_attr
+            else
+              create_element :text, text: time, attr: Style.default.message_time_attr
+              create_element :text, text: ' '
+              create_element :text, text: name, attr: Style.default.message_author_attr
+
+              if error
+                create_element :text, text: ' '
+                create_element :text, text: 'x', attr: Style.default.message_error_attr
+              end
             end
           end
         end
