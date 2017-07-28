210075a14b117008228b0e542f42a81fd3d0d45c
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-28 13:48:00 +0000

    Curses::React::Nodes::Line passes max width to child nodes

--- lib/curses/react/nodes/line.rb
+++ lib/curses/react/nodes/line.rb
@@ -11,6 +11,17 @@ module Curses
           self.width = width
         end
 
+        def children
+          left = 0
+          props[:children].map do |child_element|
+            node_klass = Nodes.klass_for child_element
+            raise "#{self.class} can only have children of type #{Text}" unless node_klass <= Text
+            child_node = node_klass.new child_element, @window, max_width: width - left
+            left += child_node.width
+            child_node
+          end
+        end
+
       private
 
         def width=(value)
--- lib/curses/react/nodes/text.rb
+++ lib/curses/react/nodes/text.rb
@@ -6,13 +6,20 @@ module Curses
   module React
     module Nodes
       class Text
-        def initialize(element, window)
+        attr_reader :max_width
+
+        def initialize(element, window, max_width:)
           @element = element
           @window = window
+          self.max_width = max_width
         end
 
         def props
           @element.all_props
+        end
+
+        def width
+          [props[:text].length, max_width].min
         end
 
         def draw
@@ -29,6 +36,13 @@ module Curses
 
         def addstr(s)
           @window.addstr s
+        end
+
+      private
+
+        def max_width=(value)
+          raise TypeError, "expected max width to be an #{Integer}" unless value.is_a? Integer
+          @max_width = value
         end
       end
     end
