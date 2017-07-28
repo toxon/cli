046d6f9368aa1a86349790f870785f4a5c3c2dab
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-28 08:47:47 +0000

    Request key prop from parent element

--- lib/curses/react/element.rb
+++ lib/curses/react/element.rb
@@ -3,17 +3,29 @@
 module Curses
   module React
     class Element
-      def self.create(type, props = {}, &block)
-        element = new type, props, &block
+      KEY_REQUEST = :key
 
-        begin
-          Fiber.yield element
-        rescue FiberError
-          element
-        end
+      private_class_method :new
+
+      def self.create(type, props = {}, &block)
+        props = props.dup
+        props[:key] ||= yield_key_request
+        yield_result new type, props, &block
       end
 
-      attr_reader :type, :props
+      def self.yield_key_request
+        Fiber.yield KEY_REQUEST
+      rescue FiberError
+        0
+      end
+
+      def self.yield_result(result)
+        Fiber.yield result
+      rescue FiberError
+        result
+      end
+
+      attr_reader :type, :props, :key
 
       def initialize(type, props = {})
         self.type  = type
@@ -26,9 +38,10 @@ module Curses
           end
 
           loop do
-            child = fiber.resume
-            break if child.nil?
-            add_child child
+            data = fiber.resume
+            data = fiber.resume children.size if data == KEY_REQUEST
+            break if data.nil?
+            add_child data
           end
         end
 
@@ -46,26 +59,31 @@ module Curses
         children << child
       end
 
-      def children=(value)
-        value.each { |child| add_child child }
-        children.freeze
-      end
-
       def type=(value)
         return @type = value if value.is_a? Symbol
 
         raise TypeError,     "expected type to be a #{Class}"        unless value.is_a? Class
         raise ArgumentError, "expected type to inherit #{Component}" unless value < Component
+
         @type = value
       end
 
       def props=(value)
         raise TypeError, "expected props to be a #{Hash}" unless value.is_a? Hash
 
-        value = value.dup
+        self.key      = value.delete :key      if value.key? :key
         self.children = value.delete :children if value.key? :children
 
         @props = value.freeze
+      end
+
+      def key=(value)
+        @key = value.freeze
+      end
+
+      def children=(value)
+        value.each { |child| add_child child }
+        children.freeze
       end
     end
   end
