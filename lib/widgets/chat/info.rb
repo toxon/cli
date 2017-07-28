b9db7d7325bcc5e551a738f3496fc823bd48ea91
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-28 13:59:14 +0000

    Remove unnecessary props

--- lib/widgets/chat/info.rb
+++ lib/widgets/chat/info.rb
@@ -19,41 +19,31 @@ module Widgets
         create_element :lines, width: props[:width] do
           create_element :line do
             create_element :text,
-                           x: 0,
                            y: 0,
-                           width: status_text.length,
                            text: status_text,
                            attr: status_attr
 
             create_element :text, x: status_text.length, y: 0, width: 1, text: ' '
 
             create_element :text,
-                           x: status_text.length + 1,
                            y: 0,
-                           width: props[:name].length,
                            text: props[:name],
                            attr: Style.default.peer_info_name_attr
 
-            create_element :text, x: status_text.length + 1 + props[:name].length, y: 0, width: 3, text: ' : '
+            create_element :text, y: 0, text: ' : '
 
             create_element :text,
-                           x: status_text.length + 1 + props[:name].length + 3,
                            y: 0,
-                           width: props[:width] - (status_text.length + 1 + props[:name].length + 3),
                            text: props[:status_message]
           end
 
           create_element :line do
             create_element :text,
-                           x: 0,
                            y: 1,
-                           width: PUBLIC_KEY_LABEL.length,
                            text: PUBLIC_KEY_LABEL
 
             create_element :text,
-                           x: PUBLIC_KEY_LABEL.length,
                            y: 1,
-                           width: props[:width] - PUBLIC_KEY_LABEL.length,
                            text: props[:public_key]
           end
         end
